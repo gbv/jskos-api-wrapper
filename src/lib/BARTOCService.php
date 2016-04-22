e972df1e0732867198d8ca5f1ebf6385856fe366
Jakob Voss <voss@gbv.de>
2016-04-22 10:01:11 +0200

    Add basic BARTOC search as proof of concept

--- src/lib/BARTOCService.php
+++ src/lib/BARTOCService.php
@@ -27,55 +27,79 @@ function getUris( $subject, $predicate, $pattern = null ) {
 
 class BARTOCService extends Service {
     
-    protected $supportedParameters = [];
+    protected $supportedParameters = ['search'];
 
     public function query($query) {
-
-        if (isset($query['uri']) and
-            preg_match('/^http:\/\/bartoc\.org\/en\/node\/([0-9]+)$/', $query['uri'])) {
-            $uri = $query['uri'];
-
-            try { 
-                $rdf = EasyRdf_Graph::newAndLoad($uri); 
-                $bartoc = $rdf->resource($uri);
-                if (!$bartoc) {
-                    return;
-                } 
-            } catch( Exception $e ) {
-                return;
-            }
-
-            $scheme = new ConceptScheme(['uri' => $uri]);
-
-            # url
-            foreach ( getUris($bartoc, 'foaf:page') as $url ) {
-                if (substr($url,0,26) != 'http://bartoc.org/en/node/') {
-                    $scheme->url = $url;
-                }
-            }
-
-            # Wikidata item
-            foreach ( getUris($bartoc, 'dc:relation', '/^http:\/\/www\.wikidata\.org\/entity\/Q[0-9]+$/') as $uri ) {
-                $scheme->identifier = [ $uri ];
-            }
-
-            # prefLabel and notation (FIXME: language is not always English)
-            foreach ($bartoc->allLiterals('schema:name') as $name) {
-                $name = $name->getValue();
-                if (preg_match('/^[A-Z]{2,5}$/', $name)) {
-                    $scheme->notation = [ $name ];
-                } else {
-                    $scheme->prefLabel = [ 'en' => $name ];
-                }
-            }
-
-            # FIXME: schema:about in RDFa output of BARTOC is malformed!
-
-            return new Page([$scheme]);
-        } else {
-            return new Page();
+        if (isset($query['uri'])) {
+            return $this->lookupByURI($query['uri']);
+        } elseif (isset($query['search'])) {
+            return new Page( $this->search($query['search']) );
         }
     }
+
+    public function lookupByURI($uri) {
+        if ( !preg_match('/^http:\/\/bartoc\.org\/en\/node\/([0-9]+)$/', $uri) ) {
+            return;
+        }
+
+        try { 
+            $rdf = EasyRdf_Graph::newAndLoad($uri); 
+            $bartoc = $rdf->resource($uri);
+            if (!$bartoc) {
+                return;
+            } 
+        } catch( Exception $e ) {
+            return;
+        }
+
+        $scheme = new ConceptScheme(['uri' => $uri]);
+
+        # url
+        foreach ( getUris($bartoc, 'foaf:page') as $url ) {
+            if (substr($url,0,26) != 'http://bartoc.org/en/node/') {
+                $scheme->url = $url;
+            }
+        }
+
+        # Wikidata item
+        foreach ( getUris($bartoc, 'dc:relation', '/^http:\/\/www\.wikidata\.org\/entity\/Q[0-9]+$/') as $uri ) {
+            $scheme->identifier = [ $uri ];
+        }
+
+        # prefLabel and notation (FIXME: language is not always English)
+        foreach ($bartoc->allLiterals('schema:name') as $name) {
+            $name = $name->getValue();
+            if (preg_match('/^[A-Z]{2,5}$/', $name)) {
+                $scheme->notation = [ $name ];
+            } else {
+                $scheme->prefLabel = [ 'en' => $name ];
+            }
+        }
+
+        # FIXME: schema:about in RDFa output of BARTOC is malformed!
+
+        return $scheme;
+    }
+
+    /**
+     * Basic search as proof of concept. Of little use without a Drupal search API.
+     */
+    private function search($search) {
+        $query = 'en/autocomplete_filter/title/title_finder/page/0/'.$search;
+        $url = 'http://bartoc.org/index.php?' . http_build_query(['q'=>$query]);
+        try {
+            $json = @json_decode( @file_get_contents($url) );
+            $schemas = [];
+            foreach ( $json as $key => $value ) {
+                # unfortunately IDs are not included in the result!
+                $schemas[] = new ConceptScheme(['prefLabel' => ['en' => $key]]);
+            }
+            return $schemas;
+        } catch (Exception $e) {
+            error_log($e);
+            return [];
+        }
+    } 
 }
 
  
